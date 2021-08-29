1fb4d38c907af1f38fb23b971ad0d8c58939eece
Mahmudul Hasan <17101120@uap-bd.edu>
2021-08-29 14:31:56 +0600

    validated

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-// Functions 
+//Display data which has beeb fetched
+let showData = (data,text)=>{
+    if (data === null){
+        let errorDiv = document.getElementById('messages');
+        
+        let message = document.createElement('div');
+        message.innerHTML=`<p class='text-center p-3  container '>Your searching result <b>${text}</b> is not found</p>`;
+        errorDiv.appendChild(message);
+    }
+    else{
+       console.log("yes");
+    }
+    
+}
+// Common Function For URL Factch
+let fetchUrlWithText = text =>{
+    if (text.length==1){
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${text}`;
+    }
+    else{
+        url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`;
+
+    }
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> showData(data.meals,text));
+}
+// Use Search Text 
 let useSearchText = text =>{
     if(text.length<1){
-        console.log('Empty');
-        let errorDiv = document.getElementById('error-message');
+        
+        let errorDiv = document.getElementById('messages');
         
         let message = document.createElement('div');
         message.innerHTML=`<p class='text-center p-3 bg-danger container text-white'><b>Please enter a meal name...</b></p>`;
@@ -10,14 +37,8 @@ let useSearchText = text =>{
 
     }
     else{
-        if(text.length == 1){
-            url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${text}`;
-            console.log(url);
-        }
-        else{
-            url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`;
-            console.log(url);
-        }
+        fetchUrlWithText (text);
+              
         
     }
 }
@@ -25,7 +46,7 @@ let useSearchText = text =>{
 // Button Click Handler 
 document.getElementById('search-button').addEventListener('click',function(){
 let searchText = document.getElementById('search-field').value;
-document.getElementById("error-message").innerHTML = "";
+document.getElementById("messages").innerHTML = "";
 useSearchText(searchText);
 
 })
